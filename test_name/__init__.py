7ad164d34e44e7cfd361f99e7f3ea4e1d6596c74
Cian Kelly <cianrkelly23@gmail.com>
2024-12-20 12:52:23 +0100

    back to case insensitive friendly folder name;

--- test_name/__init__.py
+++
@@ -1,3 +0,0 @@
-from .logger_config import LoggerConfig
-from .reader import ChlObsData, Configuration, DownloadData
-from .post_process import inspectDataStructure, checkDataStructure, chlDataHandler
